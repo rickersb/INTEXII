41ef75b904fc51480fdbe167c065a94ef78b8d27
tsteele1432 <tsteele842@gmail.com>
2022-04-04 17:40:28 -0600

    bob

---
+++ Intex/Models/County.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex.Models
+{
+    public class County
+    {
+        [Key]
+        [Required]
+        public int COUNTY_ID { get; set; }
+        [Required]
+        public string COUNTY_NAME { get; set; }
+    }
+}
--- Intex/Models/CrashesDbContext.cs
+++ Intex/Models/CrashesDbContext.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Intex.Controllers
+namespace Intex.Models
 {
     public class CrashesDbContext : DbContext
     {
-        public CrashesDbContext(DbContextOptions<CrashesDbContext> options) : base (options)
+        public CrashesDbContext(DbContextOptions<CrashesDbContext> options) : base(options)
         {
 
         }
 
-        // public DbSet<Crash> Crashes { get; set; }
+        public DbSet<Crash> Crashes { get; set; }
     }
 }
