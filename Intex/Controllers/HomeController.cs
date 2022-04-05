2de1f85f49777f7311fa1460f7e9db8b36260a32
ehorlacher <90493176+ehorlacher@users.noreply.github.com>
2022-04-05 10:50:34 -0600

    Controller stuff

--- Intex/Controllers/HomeController.cs
+++ Intex/Controllers/HomeController.cs
@@ -6,23 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Intex.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Intex.Controllers
 {
     public class HomeController : Controller
     {
-        private CrashesDbContext _context { get; set; }
+        private iCrashesRepository repo { get; set; }
 
-        //Constructor
-        public HomeController(CrashesDbContext temp)
+
+     
+        public HomeController(iCrashesRepository temp)
         {
-            _context = temp;
+            repo = temp;
         }
+
+   
         public IActionResult Index()
         {
-            var blah = _context.Crashes.Single(x => x.CRASH_ID == 10805517);
-            return View(blah);
+            /*var x = repo.Crashes
+                .ToList();*/
+
+            return View();
         }
+
         public IActionResult Graphs()
         {
             return View();
@@ -33,9 +40,9 @@ namespace Intex.Controllers
         }
         public IActionResult Summary()
         {
-            var blah = _context.Crashes.Single(x => x.CRASH_ID == 10805517);
+            var x = repo.Crashes.ToList();
 
-            return View(blah);
+            return View(x);
         }
 
         public IActionResult Privacy()
@@ -43,10 +50,60 @@ namespace Intex.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        //form for creating and editing
+        [HttpGet]
+        public IActionResult Create()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Crash c)
+        {
+            if (ModelState.IsValid)
+            {
+                if (c.CRASH_ID > 0)
+                {
+                    repo.SaveCrash(c);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    repo.CreateCrash(c);
+                    return RedirectToAction("Confirmation");
+                }
+
+            }
+            else
+            {
+
+                return View();
+            }
+        }
+
+        public IActionResult Edit(int crashid)
+        {
+            var crash = repo.Crashes
+                 .Single(x => x.CRASH_ID == crashid);
+
+
+            return View("Create", crash);
+        }
+
+
+        //delete a record
+        public IActionResult Delete(int crashid)
+        {
+            var crash = repo.Crashes
+                 .Single(x => x.CRASH_ID == crashid);
+            repo.DeleteCrash(crash);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Confirmation() => View();
+
+
+
     }
 }
+
